7400d1cea5c37259ca04042c5ec9f816b4e611a5
Lauren Hallett <hallett@uoregon.edu>
2019-07-25 23:11:45 -0700

    tsvr for the plantago-sitanion scenario

--- Data-cleaning/times-since-gopher.R
+++ Data-cleaning/times-since-gopher.R
@@ -1,5 +1,5 @@
 source("Data-cleaning/cover-cleaning.R")
-
+library(tsvr)
 
 #A function that calculates SE
 calcSE<-function(x){
@@ -112,5 +112,117 @@ ggplot(subset(plst3, rowdif < 9), aes(x=rowdif, y=cover, color = species)) + geo
   theme_classic() + labs(y="Percent cover", x = "Time point", color ="Species") + theme(text = element_text(size =14))
 ggsave("Plantago-Sitanion_gopher-recover.pdf", width = 6, height = 5)
 
-plstall <- tog %>%
-  filter(species == "PLER" | species == "SIJU") 
+plstall <- JRcover %>%
+  filter(species == "PLER" | species == "SIJU",
+         treatment != "r")
+  
+plstall2 <- plstall %>%
+  group_by(quadID, species) %>%
+  mutate(totalcover = sum(cover)) %>%
+  tbl_df() %>%
+  group_by(quadID) %>%
+  mutate(mincover = min(totalcover)) %>%
+  filter(mincover > 0)
+
+
+outnames<-c("quadID", "treatment", "classicVR", "longVR", "shortVR")
+siteout<-as.data.frame(matrix(nrow=0, ncol=5))
+names(siteout)<-outnames
+quads <- unique(plstall2$quadID)
+
+for (i in 1:length(quads)){
+  
+subber <- subset(plstall2, quadID == quads[i]) %>%
+  tbl_df()
+
+subber2 <- subber %>%
+  select(year, species, cover) %>%
+  spread(species, cover, fill = 0)
+
+d <- t(as.matrix(subber2[,2:dim(subber2)[2]]))
+
+subdat <-subber %>%
+  select(quadID, treatment) %>%
+  unique()
+
+res0 <- vreq_classic(d)
+subdat$classicVR <- res0[[3]]
+
+res <-tsvreq_classic(d)
+
+aggresLong<-aggts(res,res$ts[res$ts>=4])
+aggresShort<-aggts(res,res$ts[res$ts<4])
+
+subdat$longVR <- aggresLong[[3]]
+subdat$shortVR <- aggresShort[[3]]
+
+siteout<-rbind(siteout, subdat)
+
+}
+
+siteout2 <- siteout %>%
+  gather(metric, value, classicVR:shortVR) %>%
+  group_by(treatment, metric) %>%
+  summarize(meanval = mean(value), seval = sd(value)/sqrt(n()))
+siteout2$metric2 <- c("Classic", "Long 
+Time-Scale", "Short 
+Time-Scale", "Classic", "Long 
+Time-Scale", "Short 
+Time-Scale")
+siteout2$facorder <- c(3,2,1, 3, 2,1)
+siteout2$treatment2 <- c(rep("Control", 3), rep("Gopher Excluded", 3))
+
+
+#ggplot(siteout2, aes(x=metric, y = value, color = metric)) + geom_boxplot() + facet_wrap(~treatment)
+ggplot(siteout2, aes(x=reorder(metric2, facorder), y = meanval, color = metric)) +   
+  geom_hline(yintercept = 1, color = "grey", lty = "dashed") + 
+geom_point(size = 3) + 
+  facet_wrap(~treatment2) +
+  geom_errorbar(aes(ymin = meanval - seval, ymax = meanval + seval), width = .2) + ylim(0.75,1.25) +
+  theme_bw() + theme(legend.position = "none", text = element_text(size = 14)) + 
+  scale_color_manual(values = c( "turquoise4", "darkgreen",  "darkblue"))  + 
+  labs(x = "", y="Variance Ratio")
+ggsave("Plantago-Sitanion_tsvr_alldat.pdf", width = 8, height = 5)  
+
+
+
+
+
+
+demo <- subset(plst3, rowdif < 9) %>%
+  select(rowdif, species, cover) %>%
+  spread(species, cover)
+
+d <- t(as.matrix(demo[,2:dim(demo)[2]]))
+
+
+res0 <- vreq_classic(d)
+classicVR <- res0[[3]]
+
+res <-tsvreq_classic(d)
+
+aggresLong<-aggts(res,res$ts[res$ts>=4])
+aggresShort<-aggts(res,res$ts[res$ts<4])
+
+longVR <- aggresLong[[3]]
+shortVR <- aggresShort[[3]]
+
+demoout <- data.frame(rbind(classicVR, longVR, shortVR))
+names(demoout) = "value"
+demoout$metric <- row.names(demoout)
+demoout$metric2 <- c("Classic", "Long 
+Time-Scale", "Short 
+Time-Scale")
+demoout$facorder <- c(3,2,1)
+
+ggplot(demoout, aes(x=reorder(metric2, facorder), y=value, color = metric2)) + 
+  geom_point(size = 3) + ylim(.5, 2) +
+  theme_bw() + 
+  theme(legend.position = "none", text = element_text(size = 14)) + labs(y="Variance Ratio", x = "") + 
+  geom_hline(yintercept = 1, color = "grey", lty = "dashed") + 
+  scale_color_manual(values = c( "turquoise4", "darkgreen",  "darkblue")) 
+ggsave("Plantago-Sitanion_tsvr_demodat.pdf", width = 5, height = 5)  
+
+class(res)
+
+d<-t(as.matrix(JRGdat[,2:dim(JRGdat)[2]]))
