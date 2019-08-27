38f3870d4f71b458a20585ec2a949b5b293eb225
Lauren Hallett <hallett@uoregon.edu>
2019-08-27 08:43:04 -0700

    time since gopher

--- Data-cleaning/times-since-gopher.R
+++ Data-cleaning/times-since-gopher.R
@@ -113,6 +113,83 @@ ggplot(subset(plst3, rowdif < 9), aes(x=rowdif, y=cover, color = species)) + geo
   theme_classic() + labs(y="Percent cover", x = "Time point", color ="Species") + theme(text = element_text(size =14))
 ggsave("Plantago-Sitanion_gopher-recover.pdf", width = 6, height = 5)
 
+
+### only in never disturbed plots
+
+plst <- tog %>%
+  filter(species == "PLER" | species == "SIJU") %>%
+  mutate(keepdat = ifelse(contyear == 0 & cover > 3, 1, 0)) %>%
+  mutate(maxkeep = max(keepdat)) %>%
+  filter(maxkeep > 0) %>%
+  mutate(keepdat2 = ifelse(contyear == 0 & disturb == 0, 1, 0)) %>%
+  mutate(maxkeep2 = max(keepdat2))  %>%
+  filter(maxkeep2 > 0) %>%
+  group_by()
+           
+           
+           ifelse(ncount < 4)) 
+  
+  # mutate(keepdat = ifelse(rowdif == 0 & species == "PLER" & cover > 5, 1, 0),
+  #        keepdat = ifelse(rowdif == 0 & species == "SIJU" & cover > 5, 1, keepdat)) %>%
+  # mutate(keepdat2 = ifelse(timepoint == 0 & species == "PLER" & cover < 5, 1, 0),
+  #        keepdat2 = ifelse(timepoint == 0 & species == "SIJU" & cover < 4, 1, keepdat2)) %>%
+  group_by(repnum2, species, quadID) %>%
+  mutate(maxkeep = max(keepdat)) %>%
+  filter(maxkeep > 0) %>%
+  tbl_df() %>%
+  filter(ncount > 4) #%>%
+# filter(keepdat2 > 0)
+
+plst2 <- plst %>%
+  group_by(rowdif, quadID, species, treatment) %>%
+  summarize(meancover = mean(cover)) %>%
+  tbl_df() %>%
+  group_by(rowdif, species) %>%
+  summarize(cover = mean(meancover), secover = calcSE(meancover)) 
+
+ggplot(plst2, aes(x=rowdif, y=cover, color = species)) + geom_point()  +
+  geom_line() + xlim(0,8) + facet_wrap(~species)
+
+plst3 <- plst %>%
+  filter(species == "PLER" | species == "SIJU") %>%
+  group_by(rowdif, quadID, species, treatment) %>%
+  summarize(meancover = mean(cover)) %>%
+  tbl_df() %>%
+  group_by(species, quadID) %>%
+  # mutate(maxcover = ifelse(rowdif == 0, meancover, NA),
+  #        maxcover = max(maxcover, na.rm=T),
+  #        meancover = (meancover -maxcover)/maxcover) %>%
+  tbl_df() %>%
+  group_by(rowdif, species) %>%
+  summarize(cover = mean(meancover), secover = calcSE(meancover)) 
+
+
+ggplot(subset(plst3, rowdif < 9), aes(x=rowdif, y=cover, color = species)) + geom_point() +
+  geom_hline(yintercept = 1, color = "grey", lty = "dashed") +
+  geom_line() +
+  geom_errorbar(aes(ymin = cover - secover, ymax = cover + secover), width = .2)
+
+
+plst3 <- plst %>%
+  filter(species == "PLER" | species == "SIJU") %>%
+  group_by(rowdif, quadID, species, treatment) %>%
+  summarize(meancover = mean(cover)) %>%
+  tbl_df() %>%
+  group_by(rowdif, species) %>%
+  summarize(cover = mean(meancover), secover = calcSE(meancover)) 
+
+
+ggplot(subset(plst3, rowdif < 9), aes(x=rowdif, y=cover, color = species)) + geom_point() + 
+  geom_vline(xintercept = 1, color = "lightgrey", lwd = 10) + 
+  geom_hline(yintercept = 9.47, color = "darkgrey", lty = "dashed") +
+  geom_hline(yintercept = 23.0, color = "darkgrey", lty = "dashed") +
+  geom_line() +
+  geom_errorbar(aes(ymin = cover - secover, ymax = cover + secover), width = .2) + 
+  theme_classic() + labs(y="Percent cover", x = "Time point", color ="Species") + theme(text = element_text(size =14))
+ggsave("Plantago-Sitanion_gopher-recover.pdf", width = 6, height = 5)
+
+
+## all data
 plstall <- JRcover %>%
   filter(species == "PLER" | species == "SIJU",
          treatment != "r")
@@ -244,6 +321,17 @@ plmi2 <- plmi %>%
   summarize(cover = mean(meancover), secover = calcSE(meancover)) 
 
 ggplot(plmi2, aes(x=rowdif, y=cover, color = species)) + geom_line()
+
+
+
+ggplot(subset(plmi2, rowdif < 20), aes(x=rowdif, y=cover, color = species)) + geom_point() + 
+  geom_vline(xintercept = 1, color = "lightgrey", lwd = 10) + 
+  geom_hline(yintercept = 9.47, color = "darkgrey", lty = "dashed") +
+  geom_hline(yintercept = 23.0, color = "darkgrey", lty = "dashed") +
+  geom_line() +
+  geom_errorbar(aes(ymin = cover - secover, ymax = cover + secover), width = .2) + 
+  theme_classic() + labs(y="Percent cover", x = "Time point", color ="Species") + theme(text = element_text(size =14))
+ggsave("Plantago-Microseris_gopher-recover.pdf", width = 6, height = 5)
 
 
 demo <- subset(plmi2, rowdif < 9) %>%
@@ -387,6 +475,7 @@ ggplot(siteout3, aes(x=reorder(metric2, facorder), y = meanval, color = metric))
   theme_bw() + theme(legend.position = "none", text = element_text(size = 14)) + 
   scale_color_manual(values = c( "turquoise4", "darkgreen",  "darkblue"))  + 
   labs(x = "", y="Variance Ratio")
+ggsave("Plantago-Microseris_tsvr_demodat.pdf", width = 5, height = 5)  
 
 
 ggplot(quadinfo, aes(x=yrsdist)) + geom_histogram() + facet_wrap(~treatment)
